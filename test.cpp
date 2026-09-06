6bc1725f840a5252c700c0476377791c7418a2ad
MRNOBODY247246 <guojinwen2007@163.com>
2026-09-06 12:41:37 +0800

    4th one

--- test.cpp
+++ test.cpp
@@ -1,8 +1,11 @@
 cat >test.cpp <<EOF
-#include <iostream>
-int main() 
+#include<iostream>
+using namespace std;
+int main()
 {
-    cout<<9/8<<4*(6+3)%5<<(4*6+3)%5<<endl;
-    return 0;
+    cout<<"9/8="<<9/8<<" "
+        <<"4*(6+3)%5="<<4*(6+3)%5<<" "
+        <<"(4*6+3)%5="<<(4*6+3)%5<<endl;
+        return 0;
 }
 EOF

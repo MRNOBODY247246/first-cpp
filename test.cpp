19a93bdad43a3380392cd5c2551334bef42a9fee
MRNOBODY247246 <guojinwen2007@163.com>
2026-09-07 12:32:42 +0800

    5th onr

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
-cat >test.cpp <<EOF
+cat>test.cpp<<EOF
 #include<iostream>
-using namespace std;
+using namespce std;
 int main()
 {
-    cout<<"9/8="<<9/8<<" "
-        <<"4*(6+3)%5="<<4*(6+3)%5<<" "
-        <<"(4*6+3)%5="<<(4*6+3)%5<<endl;
-        return 0;
+    cout<<"8000秒=";
+    cout<<8000/3600<<"hour";
+    cout<<(8000%3600)/60<<"minute";
+    cout<<8000%3600%60<<"second"<<endl;
+    return 0;
 }
 EOF

f69df4549d389bb3b84769a7f6ff569a17958474
MRNOBODY247246 <guojinwen2007@163.com>
2026-09-10 12:51:38 +0800

    9

--- test.cpp
+++ test.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
-#include <iomanip>
 using namespace std;
 int main()
 {
-    cout<<"9/8="<<9/8<<"9.0/8="<<9.0/8<<"9/8.0="<<9/8.0<<"9.0/8.0="<<9.0/8.0<<endl;
-    cout<<"10.0/6.0="<<10.0/6.0<<endl;
-    cout<<"10.0/6.0="<<fixed<<setprecision(8)<<10.0/6.0<<endl;
+    int a;
+    a=65;
+    cout<<a<<endl;
     return 0;
 }

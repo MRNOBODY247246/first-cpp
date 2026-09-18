fffebd0a9a13958eda14c092721c827db50e6977
MRNOBODY247246 <guojinwen2007@163.com>
2026-09-18 14:33:10 +0800

    20

--- test.cpp
+++ test.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int main()
 {
-    float x,y;
-    x=10.5;
-    y=30.6;
-    cout<<x<<" "<<y<<endl;
-    x+=y;y=x-y;x-=y;
-    cout<<x<<" "<<y<<endl;
+    int n1,n2=5;
+    n2++;
+    cout<<"n2="<<n2<<endl;
+    ++n2;
+    cout<<"n2="<<n2<<endl;
+    n1=n2++;
+    cout<<"n1="<<n1<<" n2="<<n2<<endl;
+    n1=++n2;
+    cout<<"n1="<<n1<<" n2="<<n2<<endl;
     return 0;
 }

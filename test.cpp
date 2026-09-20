ebee8b10adeca5caf7138eeef380d90079a31574
MRNOBODY247246 <guojinwen2007@163.com>
2026-09-20 11:56:06 +0800

    22

--- test.cpp
+++ test.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main()
 {
-    float a,b,c,f;
-    a=5.5;
-    b=6.7;
-    c=9.3;
-    f=(-b+4*a*c)/(2*a);
-    cout<<f<<endl;
+    float x;
+    float y;
+    x=sqrt(5290.0/4000.0)-1;
+    y=5290*(1+x);
+    cout<<"2015年月工资："<<y<<"元";
     return 0;
 }

7b2b2173bc0a9131a3af3832d42c7a57dc1b0c43
MRNOBODY247246 <guojinwen2007@163.com>
2026-09-21 13:40:44 +0800

    23

--- test.cpp
+++ test.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
 int main()
 {
-    float x;
-    float y;
-    x=sqrt(5290.0/4000.0)-1;
-    y=5290*(1+x);
-    cout<<"2015年月工资："<<y<<"元";
+    float v0=(40/3.6),a=0.15;
+    int t=120;
+    float  vt,s;
+    vt=v0+a*t;
+    s=v0*t+0.5*a*t*t;
+    cout<<"vt="<<vt<<" s="<<s<<endl;
     return 0;
 }

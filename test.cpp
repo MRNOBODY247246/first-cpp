cd6c0de67060bae89ca51078201a9c7aca180ede
MRNOBODY247246 <guojinwen2007@163.com>
2026-09-22 16:08:12 +0800

    27

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main()
 {
-    int x,y,z;
-    x=y=1;
-    z=x++-1;
-    cout<<x<<" "<<z<<endl;
+    cout<<"ceil(3.14)="<<ceil(3.141)<<"floor(3.14)="<<floor(3.14)<<endl;
+    cout<<"4^3.0="<<pow(4,3.0)<<endl;
+    cout<<"sqrt(9)"<<sqrt(9)<<endl;
+    return 0;
 }

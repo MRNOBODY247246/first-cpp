c04763f001d73e29b8e3929d30b224d2f5731c3a
MRNOBODY247246 <guojinwen2007@163.com>
2026-09-26 12:31:12 +0800

    32

--- test.cpp
+++ test.cpp
@@ -2,12 +2,9 @@
 using namespace std;
 int main()
 {
-    int a,b,c;
-    long long s;
-    a=1562345672;
-    b=1455678343;
-    c=1234567832;
-    s=(long long)a+b+c;
-    cout<<"s="<<s<<endl;
+    int i,j;
+    i='A';
+    j='B';
+    cout<<i<<' '<<j<<'\n';
     return 0;
 }
